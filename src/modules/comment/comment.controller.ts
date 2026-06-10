9029454b28c5b309fb3e028ed231082f569fa861
Jahirul077 <jahirulislamrayhan07@gmail.com>
2026-06-10 22:02:12 +0600

    comment api work

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -3,7 +3,9 @@ import { CommentSercive } from "./comment.service";
 
 const createComment = async (req: Request, res: Response) => {
   try {
-    const result = await CommentSercive.createComment();
+    const user = req.user;
+    req.body.authorId = user?.id
+    const result = await CommentSercive.createComment(req.body);
     res.status(201).json({
         success: true,
         message: "Comment created successfully",
