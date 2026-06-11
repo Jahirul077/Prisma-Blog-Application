52b1ad758c1e87e243466b86e0e111b0cd117f92
Johirul <jahirulislamrayhan07@gmail.com>
2026-06-11 08:58:20 +0600

    prisma issue computer

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,15 +1,15 @@
 import type { Request, Response } from "express";
-import { CommentSercive } from "./comment.service";
+import { CommentService } from "./comment.service";
 
 const createComment = async (req: Request, res: Response) => {
   try {
     const user = req.user;
-    req.body.authorId = user?.id
-    const result = await CommentSercive.createComment(req.body);
+    req.body.authorId = user?.id;
+    const result = await CommentService.createComment(req.body);
     res.status(201).json({
-        success: true,
-        message: "Comment created successfully",
-        data: result
+      success: true,
+      message: "Comment created successfully",
+      data: result,
     });
   } catch (error: any) {
     res.status(400).json({
@@ -20,5 +20,5 @@ const createComment = async (req: Request, res: Response) => {
 };
 
 export const CommentController = {
-    createComment
-}
+  createComment,
+};
