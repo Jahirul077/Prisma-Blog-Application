84e34c71fdf584e3c6f29fbf62a6f62b742e65c0
Johirul <jahirulislamrayhan07@gmail.com>
2026-06-17 08:52:27 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import cors from "cors";
 import { commentRouter } from "./modules/comment/comment.router";
+import globalErrorHandler from "./middlewares/globalErrorhandler";
+import { notFound } from "./middlewares/notFound";
 
 const app: Application = express();
 
@@ -19,10 +21,15 @@ app.use(express.json());
 
 app.use("/posts", postRouter);
 
+
 app.use("/comments", commentRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World from Prisma Blog Application");
 });
+
+app.use(notFound)
+app.use(globalErrorHandler)
+
 
 export default app;
